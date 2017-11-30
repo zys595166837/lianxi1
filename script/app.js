102b51ebc362cc466f3f47d11a6142985def5b9d
little_fairy <zhang@qq.com>
2017-11-30 12:00:17 +0800

    第一天上午结束
    主要功能:修改toggle该方法,后续继续

--- script/app.js
+++ script/app.js
@@ -1,13 +1,27 @@
 /*
 * @Author: Administrator
 * @Date:   2017-11-30 11:23:29
-* @Last Modified by:   Administrator
-* @Last Modified time: 2017-11-30 11:25:56
+* @Last Modified by:   little_fairy
+* @Last Modified time: 2017-11-30 11:58:07
 */
 // 创建应用模块
 var app=angular.module("yike",[]);
 app.run(["$rootScope",function($rootScope){
+	// 在根目录绑定collapse属性,初始值为false
+		//表示nav,header,body没有弹出
+	$rootScope.collapse=false;
 	$rootScope.toggle=function(){
-		alert(111)
+		// alert(111)
+		$rootScope.collapse=!$rootScope.collapse;
+		// 将导航栏的小标题添加显示和隐藏效果
+		// 获取所有的dd
+		var dds=document.querySelectorAll("dd");
+		循环遍历每一个dd,修改dd的样式达到隐藏的效果
+		if ($rootScope.collapse) {
+			// 如果值为true,显示,
+			
+		}else{//false.隐藏
+
+		}
 	}
 }])
